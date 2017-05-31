4a807db61739f2040ade70d8cd1eae636cbcf6fb
Unknown <rc4350@ship.edu>
2017-05-30 22:52:33 -0400

    Tests

---
+++ src/PracticeClass1.java
@@ -0,0 +1,11 @@
+
+public class PracticeClass1
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hellow World");
+
+	}
+
+}
---
+++ test/Test1.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test1
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
