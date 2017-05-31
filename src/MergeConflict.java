73f3f24a7b128e007e477246dddd4351dd31a94b
Unknown <rc4350@ship.edu>
2017-05-31 10:19:08 -0400

    added MergeConflict class

---
+++ src/MergeConflict.java
@@ -0,0 +1,22 @@
+
+public class MergeConflict
+{
+	int data;
+	String name;
+	
+	public void nameMethod()
+	{
+		
+	}
+	
+	public void dataMethod()
+	{
+		
+	}
+	
+	@Override
+	public String toString()
+	{
+		return name+data;
+	}
+}
